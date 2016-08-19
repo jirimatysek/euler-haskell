2187772643e269e1998899ffee8d8370b9a6e7a5
Jiri Matysek <jmatysek@vendavo.com>
2016-08-19 15:17:37 +0200

    Euler problem 2

--- Euler/ProjectEuler.hs
+++ Euler/ProjectEuler.hs
@@ -1,9 +1,8 @@
 module Euler.ProjectEuler 
     where
 
-
-
 -- Problem 1
+-- Multiples of 3 and 5
 problem001 :: Integer -> Integer
 problem001 n = sumOfN 3 + sumOfN 5 - sumOfN 15
     where 
@@ -11,3 +10,10 @@ problem001 n = sumOfN 3 + sumOfN 5 - sumOfN 15
             let a = ((n - 1) `div` x)
             in x * ((a * (a + 1)) `div` 2)
 
+-- Problem 2
+-- Even Fibonacci numbers
+problem002 :: Integer -> Integer
+problem002 n = sum $ takeWhile (<n) $ filter even fibSeq
+
+fibSeq :: [Integer]
+fibSeq = 1 : 1 : zipWith (+) fibSeq (tail fibSeq)
